066b99fc871cab04e5097c933060658995bbefeb
Ganesh Prasad Preetham <gpreetham580@gmail.com>
2023-02-07 23:58:42 +0530

    Initial Check-in
    Catalog.API --> Running "docker-compose -f docker-compose.yml -f docker-compose-override.yml up -d " inside the project directory will set up the docker container with all the required dependencies

---
+++ Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,29 @@
+using Catalog.API.Data;
+using Catalog.API.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
